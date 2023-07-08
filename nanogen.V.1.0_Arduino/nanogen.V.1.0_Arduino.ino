e6e8fed5f57d4d1fd0c3e168707b76473ce5fbaa
douribun <jus.kim@daum.net>
2023-07-08 15:28:19 +0900

    updating 0.3

--- nanogen.V.1.0_Arduino/nanogen.V.1.0_Arduino.ino
+++ nanogen.V.1.0_Arduino/nanogen.V.1.0_Arduino.ino
@@ -1,84 +1,70 @@
-//const byte waterPump=D5;const byte airPump=D6;const byte gasPump=D7;
-const byte relayPin[] = {D5, D6, D7, D8};
-byte len = sizeof(relayPin);
-const byte airOn[] = {LOW, LOW, HIGH, HIGH};   //LOW=ON
-const byte gasOn[] = {LOW, HIGH, LOW, HIGH};
+int pins[] = {D5,D6,D7};      //esp8266 {D5,D6,D7}, {waterPump, airPump, gasPump};
+int relayNo;
+uint16_t delayTime=5000;              //pump air 제거 공회전 시간 -> relayControl()  
 
-class Relay{
-private:
-  byte pin;
-  byte turn;
+//communicate with Relay=======================================================
+void relayInit(){
+  relayNo = sizeof(pins);
+  for(int i=0;i<relayNo;i++){
+    pinMode(pins[i], OUTPUT);
+    digitalWrite(pins[i], HIGH);    //Turn off all relay.
+  }  
+  delay(1000);
+}
 
-public:
-  Relay(){
-    init();
-  }
-  void init(){
-    for(int i=0;i<len;i++){
-      pinMode(relayPin[i], OUTPUT);
-      act(relayPin[i], HIGH);
+void LedTimer(){
+  for(int i=0;i<delayTime/1000;i++){
+    digitalWrite(BUILTIN_LED, LOW);
+    delay(500);
+    digitalWrite(BUILTIN_LED, HIGH);
+    delay(500);
+  }  
+}
+
+void relayControl(byte _mod, byte _req){
+  //Serial.print(_mod); Serial.print(_req);   //for check
+  if(_req==1){
+    digitalWrite(pins[0], LOW);
+    LedTimer();
+    if(_mod==0){
+      digitalWrite(pins[1], LOW);
+    }else{
+      digitalWrite(pins[2], LOW);
     }
-    pinMode(BUILTIN_LED, OUTPUT);    
-  }
-
-  byte state(byte _pin){
-    pin = _pin;
-    return digitalRead(pin);
-  }
-
-  void act(byte _pin, byte _turn){
-    pin = _pin;
-    turn = _turn;
-    if (state(pin) != turn){
-      digitalWrite(pin, turn);
-      digitalWrite(BUILTIN_LED, turn);
+    digitalWrite(BUILTIN_LED, LOW);
+  }else if(_req==0){
+    for(int i=1;i<relayNo;i++){
+      digitalWrite(pins[i], HIGH);
     }
-  }
-};
-
-class NexCmd{
-  //int charLength;
-  char buffer[10]={0,}; 
-public:
-  int charLength = Serial.readBytesUntil(';', buffer, 10);   
-  byte parseNx(){    
-    Serial.flush();
-    if(buffer[2] == 'C'){
-      return buffer[3] - '0', buffer[4] - '0';
-    }else return 0;
-  }
-};
-
-void control(Relay _rel, byte _mod, byte _req){  
-  if(_req == 1){
-    if(_mod == 0){
-      for(int i=0;i<len;i++){
-        _rel.act(relayPin[i], airOn[i]);
-      }
-    }else if(_mod == 1){
-      for(int i=0;i<len;i++){
-        _rel.act(relayPin[i], gasOn[i]);
-      }
-    }
-  }else{
-    for(int i=0;i<len;i++){
-      _rel.act(relayPin[i], HIGH);
-    }
+    LedTimer();
+    digitalWrite(pins[0], HIGH);
+    digitalWrite(BUILTIN_LED, HIGH);
   }
 }
 
-NexCmd nex1;
-Relay rel1;
-
-void setup(){  //=============================================
-  Serial.begin(115200);  
-  rel1.init();
+void parseNx(){
+  char buffer[10]={0,};
+  int charLength = Serial.readBytesUntil(';', buffer, 10);
+  Serial.flush();
+  if(buffer[2] == 'C'){
+    byte mod = buffer[3] - '0';
+    byte req = buffer[4] - '0';
+    //Serial.print(mod); Serial.print(req);     //for check
+    relayControl(mod, req);
+  }
+  //Serial.flush();
 }
 
-void loop(){//==============================================
-  if (Serial.available()) {      
-    byte mo, re = nex1.parseNx();
-    Serial.printf("Nextion data: mod=%d, req=%d\n", mo, re);
-    control(rel1, mo, re);
+void setup(){
+  Serial.begin(9600);
+  pinMode(BUILTIN_LED, OUTPUT);
+  digitalWrite(BUILTIN_LED, HIGH);  
+  relayInit();                            //delay time
+}
+
+void loop(){
+  if (Serial.available()) {
+    //Serial.printf("rcv:%s\n", buffer);
+    parseNx();
   }
 }
